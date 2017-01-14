e3fdaf607da0387e065cb1ad7d0979b520377a6d
Park Ju Hyung <qkrwngud825@gmail.com>
2017-01-15 03:33:04 +0900

    SystemUI: add burnIn protection
    Devices with AMOLED display suffer from
    status-bar's notification items and nagivation bar's software keys
    causing permanent burn-ins when used long-term.
    
    Moving all items in the area
    both horizontally and vertically workarounds this problem.
    
    jrizzoli: integrate with runtime cmsdk preference
    
    Linux4: removed toggle from settings - the burnIn protection is always
    enabled if the corresponding AOSP overlay is set to true
    
    Change-Id: I35b04d1edff86a556adb3ad349569e5d82653f16
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Thecrazyskull <anaskarbila@gmail.com>
    Signed-off-by: Joey Rizzoli <joey@lineageos.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -223,6 +223,7 @@ import com.android.systemui.statusbar.phone.panelstate.PanelExpansionChangeEvent
 import com.android.systemui.statusbar.phone.panelstate.PanelExpansionStateManager;
 import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.BrightnessMirrorController;
+import com.android.systemui.statusbar.policy.BurnInProtectionController;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
@@ -455,6 +456,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
     @Nullable
     protected LockscreenWallpaper mLockscreenWallpaper;
     private final AutoHideController mAutoHideController;
+    private BurnInProtectionController mBurnInProtectionController;
 
     private final Point mCurrentDisplaySize = new Point();
 
@@ -1194,6 +1196,12 @@ public class CentralSurfacesImpl extends CoreStartable implements
                     checkBarModes();
                 });
         initializer.initializeStatusBar(mCentralSurfacesComponent);
+
+        if (mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_enableBurnInProtection)) {
+            mBurnInProtectionController = new BurnInProtectionController(mContext,
+                    this, mStatusBarView);
+        }
 
         mStatusBarTouchableRegionManager.setup(this, mNotificationShadeWindowView);
         mHeadsUpManager.addListener(mNotificationPanelViewController.getOnHeadsUpChangedListener());
@@ -3610,6 +3618,9 @@ public class CentralSurfacesImpl extends CoreStartable implements
 
             updateNotificationPanelTouchState();
             mNotificationShadeWindowViewController.cancelCurrentTouch();
+            if (mBurnInProtectionController != null) {
+                mBurnInProtectionController.stopShiftTimer(true);
+            }
             if (mLaunchCameraOnFinishedGoingToSleep) {
                 mLaunchCameraOnFinishedGoingToSleep = false;
 
@@ -3701,6 +3712,9 @@ public class CentralSurfacesImpl extends CoreStartable implements
                 }
             }
             updateScrimController();
+            if (mBurnInProtectionController != null) {
+                mBurnInProtectionController.startShiftTimer(true);
+            }
         }
     };
 
