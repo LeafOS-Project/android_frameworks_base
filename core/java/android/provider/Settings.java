509935f97f4dbfca18fec33a8fb744f83942064b
Jesse Chan <jc@lineageos.org>
2020-05-26 21:17:59 +0800

    SystemUI: support black theme for dark mode [1/3]
    Change-Id: I57cd53de8f2c1e4d445441b514875b6af915b858
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10168,6 +10168,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_HINT = "navigation_bar_hint";
 
         /**
+         * Whether to turn on black theme
+         * @hide
+         */
+        public static final String SYSTEM_BLACK_THEME = "system_black_theme";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
