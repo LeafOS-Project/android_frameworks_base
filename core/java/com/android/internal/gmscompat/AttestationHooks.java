0c650a9b40148980b3cc2ac4240266611bd99081
SamarV-121 <samarvispute121@pm.me>
2022-09-05 08:06:31 +0000

    gmscompat: Spoof props for google app
     * unlocks weather widget
    
    Change-Id: Ib5c576e28c9672d49f185602a05c21bc3ef2d19c

--- core/java/com/android/internal/gmscompat/AttestationHooks.java
+++ core/java/com/android/internal/gmscompat/AttestationHooks.java
@@ -28,6 +28,7 @@ import java.util.Arrays;
 public final class AttestationHooks {
     private static final String TAG = "GmsCompat/Attestation";
     private static final String PACKAGE_GMS = "com.google.android.gms";
+    private static final String PACKAGE_GOOGLE = "com.google.android.googlequicksearchbox";
     private static final String SAMSUNG = "com.samsung.android.";
     private static final String FAKE_FINGERPRINT = "google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys";
 
@@ -76,8 +77,9 @@ public final class AttestationHooks {
     }
 
     public static void initApplicationBeforeOnCreate(Application app) {
-        if (PACKAGE_GMS.equals(app.getPackageName())
-                || app.getPackageName().startsWith(SAMSUNG)) {
+        if (PACKAGE_GMS.equals(app.getPackageName()) ||
+                PACKAGE_GOOGLE.equals(app.getPackageName()) ||
+                app.getPackageName().startsWith(SAMSUNG)) {
             sIsGms = true;
             spoofBuildGms();
         }
