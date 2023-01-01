e24d30dfa2847716252a263138b0789110537ddd
Danny Lin <danny@kdrag0n.dev>
2023-01-01 04:53:41 -0800

    base: Dynamically patch build fingerprint in GMS process
    "KeyStore" always seems to be used early in the attestation
    process, before the fingerprint is checked.
    
    Dynamic patching avoids problems with device detection and functionality
    that can be caused by permanently spoofing another device.
    
    References:
    - https://github.com/kdrag0n/safetynet-fix/commit/73c8587a808533c38e47f40c4485579068397205
    - https://github.com/kdrag0n/safetynet-fix/pull/261
    
    [SamarV-121: Adapt it to fw/b]
    
    Co-authored-by: Ilya Kushnir <ilya.displax@gmail.com>
    Co-authored-by: SamarV-121 <samarvispute121@pm.me>
    Change-Id: I346059e4ab9502537096f8c8aec3c48a5a32923e

--- core/java/com/android/internal/gmscompat/AttestationHooks.java
+++ core/java/com/android/internal/gmscompat/AttestationHooks.java
@@ -31,6 +31,8 @@ public final class AttestationHooks {
     private static final String SAMSUNG = "com.samsung.android.";
     private static final String FAKE_FINGERPRINT = "google/angler/angler:6.0/MDB08L/2343525:user/release-keys";
 
+    private static final long PATCH_DURATION = 2000L;
+
     private static volatile boolean sIsGms = false;
 
     private AttestationHooks() { }
@@ -59,13 +61,34 @@ public final class AttestationHooks {
                 && PROCESS_UNSTABLE.equals(processName)) {
           sIsGms = true;
           setBuildField("MODEL", Build.MODEL + " ");
-          setBuildField("FINGERPRINT", FAKE_FINGERPRINT);
         }
 
         // Samsung apps like SmartThings, Galaxy Wearable crashes on samsung devices running AOSP
         if (packageName.startsWith(SAMSUNG)) {
           setBuildField("BRAND", "google");
           setBuildField("MANUFACTURER", "google");
+        }
+    }
+
+    public static void patchBuildFp(String type) {
+        Log.d(TAG, "Get service - algorithm=" + type);
+        if (sIsGms && "KeyStore".equals(type)) {
+            String originalFp = Build.FINGERPRINT;
+            Log.d(TAG, "Patch build for KeyStore " + originalFp + " -> " + FAKE_FINGERPRINT);
+            setBuildField("FINGERPRINT", FAKE_FINGERPRINT);
+
+            Thread thread = new Thread(() -> {
+                try {
+                    Thread.sleep(PATCH_DURATION);
+                    Log.d(TAG,
+                            "Unpatch build for KeyStore " + FAKE_FINGERPRINT + " -> " + originalFp);
+                    setBuildField("FINGERPRINT", originalFp);
+                } catch (InterruptedException e) {
+                    Log.e(TAG, "Error while sleeping", e);
+                }
+            });
+            thread.setDaemon(true);
+            thread.start();
         }
     }
 
