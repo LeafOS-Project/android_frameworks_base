b456b19b020871e8476780db9636530e3da36fa1
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-16 06:00:12 +0000

    SystemUI: Make round QS toggleable [1/3]
    Change-Id: I86e3bc990b78e763bfc9fb4a7ed8ffeeff95cc3f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11172,6 +11172,12 @@ public final class Settings {
         public static final String QS_TILE_SHAPE = "qs_tile_shape";
 
         /**
+         * Quicksettings UI Style
+         * @hide
+         */
+        public static final String QS_UI_STYLE = "qs_ui_style";
+
+        /**
          * Brightness slider styles
          * @hide
          */
