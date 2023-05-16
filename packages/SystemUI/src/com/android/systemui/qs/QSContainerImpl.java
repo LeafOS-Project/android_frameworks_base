7d99bfefd9e2f0c86ba641a0e600c264485da9fb
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-16 06:00:12 +0000

    SystemUI: Make round QS toggleable [1/3]
    Change-Id: I86e3bc990b78e763bfc9fb4a7ed8ffeeff95cc3f

--- packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
@@ -17,6 +17,7 @@
 package com.android.systemui.qs;
 
 import static android.app.StatusBarManager.DISABLE2_QUICK_SETTINGS;
+import static com.android.internal.util.systemui.qs.QSStyleUtils.isRoundQS;
 
 import android.content.Context;
 import android.content.res.Configuration;
@@ -99,7 +100,7 @@ public class QSContainerImpl extends FrameLayout implements Dumpable {
         int availableHeight = View.MeasureSpec.getSize(heightMeasureSpec);
         int maxQs = availableHeight - layoutParams.topMargin - layoutParams.bottomMargin
                 - getPaddingBottom();
-        if (navBelow) {
+        if (navBelow && isRoundQS(mContext)) {
             maxQs -= getResources().getDimensionPixelSize(R.dimen.navigation_bar_height);
         }
         int padding = mPaddingLeft + mPaddingRight + layoutParams.leftMargin
