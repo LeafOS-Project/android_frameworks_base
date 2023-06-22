b4a294161b185cbf284d89028ceb54d6e2e98ade
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-22 23:11:13 +0530

    SystemUI: Add FingerprintInteractiveToAuthProvider implementation
    This is required for the "touch to unlock anytime" setting on devices
    with side mounted fingerprint sensor.
    
    Ref: 66a048d646fd816e7757e300c62fa5cebf04761d
    
    [SamarV-121]
    - Rewrite in kt and adapt to A14 QPR2 changes
    - Ref: 2e9cb6c542795ea75c794302d53dc722a385c6dd
    
    Change-Id: If3308860a428d9966f2b0a0024764df95f8f9a8b

---
+++ packages/SystemUI/src/com/android/systemui/biometrics/FingerprintInteractiveToAuthProviderImpl.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2023 ArrowOS
+ *           (C) 2024 LeafOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+package com.android.systemui.biometrics
+
+import android.content.ContentResolver
+import android.content.Context
+import android.hardware.biometrics.common.AuthenticateReason
+import android.provider.Settings
+import javax.inject.Inject
+import kotlinx.coroutines.flow.Flow
+
+class FingerprintInteractiveToAuthProviderImpl @Inject constructor(
+    private val context: Context
+) : FingerprintInteractiveToAuthProvider {
+    private val defaultValue = if (context.resources.getBoolean(
+            com.android.internal.R.bool.config_performantAuthDefault
+        )
+    ) 1 else 0
+
+    override val enabledForCurrentUser: Flow<Boolean>
+        get() = flow {
+            val resolver = context.contentResolver
+            var value: Int = Settings.Secure.getIntForUser(
+                resolver,
+                Settings.Secure.SFPS_PERFORMANT_AUTH_ENABLED,
+                -1,
+                resolver.getUserId()
+            )
+            if (value == -1) {
+                value = defaultValue
+                Settings.Secure.putIntForUser(
+                    resolver,
+                    Settings.Secure.SFPS_PERFORMANT_AUTH_ENABLED,
+                    value,
+                    resolver.getUserId()
+                )
+            }
+            emit(value == 0)
+        }
+
+    override fun getVendorExtension(userId: Int): AuthenticateReason.Vendor? =
+        null
+}
