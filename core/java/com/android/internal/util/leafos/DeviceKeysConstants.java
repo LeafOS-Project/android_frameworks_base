LMODroid/platform_frameworks_base

Reimplement hardware keys custom rebinding
Fixups for ten @sam3000

This is a squash of the following commits:

Author: Phil Tunstall <ptunstall@gmail.com>
Date:   Fri Nov 14 09:44:49 2014 -0800

    Hardware key custom rebinding (1/2)

    Framework changes to allow rebinding of the actions performed on the
    following key press events: Home long-press, home double-tap, menu press,
    menu long-press, search press, search long-press, app-switch press and
    app-switch long-press.

    The available actions are: Nothing, open/close menu, recent apps
    switcher, search assistant, voice search, in-app search, device sleep,
    and launch camera.

    Change-Id: I72c0d220a09d79230bfa299e0521ed693e5c25f1

Author: Steve Kondik <steve@cyngn.com>
Date:   Tue Oct 18 23:38:28 2016 -0700

    wm: Add support for split screen button behavior

     * And make it the default for long-press recents like the navbar.

    Change-Id: I432c80a2c9b29b9a02d64e29d484f92623b0648a

Author: Bruno Martins <bgcngm@gmail.com>
Date:   Mon Dec 25 22:10:28 2017 +0000

    Forward-port hardware keys custom rebinding to O

     * Adapted to Lineage SDK as well as to the moved hardware keys
       configs and constants.

     * Converted variables that track user-customisable behavior for
       certain key events from int to Action in order to use the newly
       introduced SDK function helpers.

Change-Id: I2e2234e3d01d943e0cadd23898288f8a88936a47

Setting a custom menu key action will do both the custom action
and the default action at the same time. Comparing code with 14.1
we're missing the return here.

Change-Id: I2f188ec78fae5068ffa1a5c80a8afa5ee2f167b5

core/java/com/android/internal/util/libremobileos/DeviceKeysConstants.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2018,2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.libremobileos;
+
+import android.content.ContentResolver;
+import android.os.UserHandle;
+
+import android.provider.Settings;
+
+public class DeviceKeysConstants {
+    // Available custom actions to perform on a key press.
+    // Must match values for KEY_HOME_LONG_PRESS_ACTION in:
+    //   frameworks/base/core/java/android/provider/Settings.java
+    public enum Action {
+        NOTHING,
+        MENU,
+        APP_SWITCH,
+        SEARCH,
+        VOICE_SEARCH,
+        IN_APP_SEARCH,
+        LAUNCH_CAMERA,
+        SLEEP,
+        LAST_APP,
+        SPLIT_SCREEN,
+        KILL_APP;
+
+        public static Action fromIntSafe(int id) {
+            if (id < NOTHING.ordinal() || id > Action.values().length) {
+                return NOTHING;
+            }
+            return Action.values()[id];
+        }
+
+        public static Action fromSettings(ContentResolver cr, String setting, Action def) {
+            return fromIntSafe(Settings.System.getIntForUser(cr,
+                    setting, def.ordinal(), UserHandle.USER_CURRENT));
+        }
+    }
+
+    // Masks for checking presence of hardware keys.
+    // Must match values in:
+    //   frameworks/base/core/res/res/values/lmodroid_config.xml
+    public static final int KEY_MASK_HOME = 0x01;
+    public static final int KEY_MASK_BACK = 0x02;
+    public static final int KEY_MASK_MENU = 0x04;
+    public static final int KEY_MASK_ASSIST = 0x08;
+    public static final int KEY_MASK_APP_SWITCH = 0x10;
+    public static final int KEY_MASK_CAMERA = 0x20;
+    public static final int KEY_MASK_VOLUME = 0x40;
+}

LeafOS-Project/android_frameworks_base

core/java/com/android/internal/util/leafos/DeviceKeysConstants.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2018,2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.leafos;
+
+import android.content.ContentResolver;
+import android.os.UserHandle;
+
+import android.provider.Settings;
+
+public class DeviceKeysConstants {
+    // Available custom actions to perform on a key press.
+    // Must match values for KEY_HOME_LONG_PRESS_ACTION in:
+    //   frameworks/base/core/java/android/provider/Settings.java
+    public enum Action {
+        NOTHING,
+        MENU,
+        APP_SWITCH,
+        SEARCH,
+        VOICE_SEARCH,
+        IN_APP_SEARCH,
+        LAUNCH_CAMERA,
+        SLEEP,
+        LAST_APP,
+        SPLIT_SCREEN,
+        KILL_APP;
+
+        public static Action fromIntSafe(int id) {
+            if (id < NOTHING.ordinal() || id > Action.values().length) {
+                return NOTHING;
+            }
+            return Action.values()[id];
+        }
+
+        public static Action fromSettings(ContentResolver cr, String setting, Action def) {
+            return fromIntSafe(Settings.System.getIntForUser(cr,
+                    setting, def.ordinal(), UserHandle.USER_CURRENT));
+        }
+    }
+
+    // Masks for checking presence of hardware keys.
+    // Must match values in:
+    //   frameworks/base/core/res/res/values/lmodroid_config.xml
+    public static final int KEY_MASK_HOME = 0x01;
+    public static final int KEY_MASK_BACK = 0x02;
+    public static final int KEY_MASK_MENU = 0x04;
+    public static final int KEY_MASK_ASSIST = 0x08;
+    public static final int KEY_MASK_APP_SWITCH = 0x10;
+    public static final int KEY_MASK_CAMERA = 0x20;
+    public static final int KEY_MASK_VOLUME = 0x40;
+}