7b9e69199b64adce4306b8aa8512aa9497a68e6f
nift4 <nift4@protonmail.com>
2023-07-15 15:42:55 +0200

    services: Software face unlock implementation
    Change-Id: I044962e3997f70a5d50a997c8d7cb173591151ef

--- services/core/java/com/android/server/biometrics/sensors/face/hidl/Face10.java
+++ services/core/java/com/android/server/biometrics/sensors/face/hidl/Face10.java
@@ -83,6 +83,8 @@ import com.android.server.biometrics.sensors.face.UsageStats;
 import com.android.server.biometrics.sensors.face.aidl.AidlResponseHandler;
 import com.android.server.biometrics.sensors.face.aidl.AidlSession;
 
+import com.libremobileos.faceunlock.client.FaceUnlockHalManager;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -466,6 +468,11 @@ public class Face10 implements IHwBinder.DeathRecipient, ServiceProvider {
             Slog.w(TAG, "NoSuchElementException", e);
         } catch (RemoteException e) {
             Slog.e(TAG, "Failed to get face HAL", e);
+        }
+
+        if (mDaemon == null) {
+            Slog.i(TAG, "Loading software Face HAL");
+            mDaemon = FaceUnlockHalManager.getIBiometricsFace();
         }
 
         if (mDaemon == null) {
