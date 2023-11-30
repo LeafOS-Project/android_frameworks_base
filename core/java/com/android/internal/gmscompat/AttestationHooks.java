ca83f9c3079f39af6d0076d1525e62c3504eb537
SamarV-121 <samar@samarv121.dev>
2023-11-30 15:41:12 +0530

    [WIP] gmscompat: Dynamically spoof props for GMS
     * since goolag is on rampage of killing working build FPs
    
    Change-Id: I1caa82d91606353c4f91c9e9a3c7ed653b7ccff5

--- core/java/com/android/internal/gmscompat/AttestationHooks.java
+++ core/java/com/android/internal/gmscompat/AttestationHooks.java
@@ -19,15 +19,33 @@ package com.android.internal.gmscompat;
 import android.app.Application;
 import android.content.Context;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.TextUtils;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.reflect.Field;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.Iterator;
 
 /** @hide */
 public final class AttestationHooks {
+    private static final Executor executor = Executors.newSingleThreadExecutor();
+    private static final Handler mainHandler = new Handler(Looper.getMainLooper());
+
     private static final String TAG = "GmsCompat/Attestation";
+    private static final String API = "https://play.samarv121.dev";
     private static final String PACKAGE_GMS = "com.google.android.gms";
     private static final String PROCESS_UNSTABLE = "com.google.android.gms.unstable";
     private static final String SAMSUNG = "com.samsung.android.";
@@ -63,6 +81,33 @@ public final class AttestationHooks {
         }
     }
 
+    private static class FetchPropsTask implements Runnable {
+        @Override
+        public void run() {
+            String responseBody = fetchProps();
+            mainHandler.post(() -> spoofBuildGms(responseBody));
+        }
+
+        private void spoofBuildGms(String responseBody) {
+            if (responseBody != null) {
+                try {
+                    JSONObject response = new JSONObject(responseBody);
+                    Iterator<String> keys = response.keys();
+
+                    while (keys.hasNext()) {
+                        String key = keys.next();
+                        String value = response.getString(key);
+                        Log.i(TAG, key + ": " + value);
+
+                        setBuildField(key, value);
+                    }
+                } catch (JSONException e) {
+                    Log.e(TAG, "Error parsing JSON response", e);
+                }
+            }
+        }
+    }
+
     public static void initApplicationBeforeOnCreate(Context context) {
         final String packageName = context.getPackageName();
         final String processName = Application.getProcessName();
@@ -75,18 +120,16 @@ public final class AttestationHooks {
         if (PACKAGE_GMS.equals(packageName)) {
             setBuildField("TIME", String.valueOf(System.currentTimeMillis()));
             if (PROCESS_UNSTABLE.equals(processName)) {
-              sIsGms = true;
-              setBuildField("FINGERPRINT", "google/marlin/marlin:7.1.2/NJH47F/4146041:user/release-keys");
-              setBuildField("PRODUCT", "marlin");
-              setBuildField("DEVICE", "marlin");
-              setBuildField("MODEL", "Pixel XL");
+                sIsGms = true;
+                executor.execute(new FetchPropsTask());
             }
         }
 
-        // Samsung apps like SmartThings, Galaxy Wearable crashes on samsung devices running AOSP
+        // Samsung apps like SmartThings, Galaxy Wearable crashes
+        // on samsung devices running AOSP
         if (packageName.startsWith(SAMSUNG)) {
-          setBuildField("BRAND", "google");
-          setBuildField("MANUFACTURER", "google");
+            setBuildField("BRAND", "google");
+            setBuildField("MANUFACTURER", "google");
         }
     }
 
@@ -99,6 +142,35 @@ public final class AttestationHooks {
         // Check stack for SafetyNet
         if (sIsGms && isCallerSafetyNet()) {
             throw new UnsupportedOperationException();
+        }
+    }
+
+    private static String fetchProps() {
+        try {
+            URL url = new URI(API).toURL();
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            try {
+                urlConnection.setConnectTimeout(10000);
+
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line).append("\n");
+                }
+
+                reader.close();
+
+                return response.toString();
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error making API request", e);
+            return null;
         }
     }
 }
